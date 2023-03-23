f757792556f6f1b4bf72cbfacfba96f50ee24841
Stephen Strange <qosimovrahmatullo006@gmail.com>
2023-03-23 09:35:43 +0500

    gfghgh

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
 var caret = document.querySelector(".caret-parent .caret");
 document.querySelectorAll(".nav-item").forEach(function (item) {
   item.addEventListener("mouseover", function (e) {
-    caret.style.width = e.target.offsetWidth - 16 + "px";
-    caret.style.left = e.target.offsetLeft + 8 + "px";
-    caret.style.opacity = 1;
+    e.stopPropagation()
+    caret.style.width = e.currentTarget.offsetWidth - 16 + "px";
+    caret.style.left = e.currentTarget.offsetLeft + 8 + "px";
   });
 });
+
+new Splide( '.splide' ).mount();
