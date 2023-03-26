54157ab86c956e9a63fb306c236057cb2d4bd162
Stephen Strange <qosimovrahmatullo006@gmail.com>
2023-03-26 18:09:07 +0500

    more

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 var caret = document.querySelector(".caret-parent .caret");
-document.querySelectorAll(".nav-item").forEach(function (item) {
+document.querySelectorAll("nav .nav-item").forEach(function (item) {
   item.addEventListener("mouseover", function (e) {
     e.stopPropagation();
     caret.style.width = e.currentTarget.offsetWidth - 16 + "px";
